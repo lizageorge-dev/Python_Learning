442155b3604480d6822ce2c7423329a0f714a223
Liza <lizasusan93@gmail.com>
2026-05-14 19:59:25 -0400

    Python Lab 1

---
+++ Python_Learning1.py
@@ -0,0 +1,18 @@
+"""
+Part 1: Written Exercise - Understanding Programming
+1.	What is computer programming, and why is it important in today’s technology-driven world?
+Computer programming is a set of instructions to build applications .Everything in todays world depends on technology from learning to shopping, so programming is the key for developing these applications.
+2.	Briefly describe what Python is and one reason it is a popular programming language for beginners.
+Python is a programming language to build various applications. It is popular for beginners because syntax are readable and easy to write the code.
+
+3.	What do you need (software/tools) to start writing and running Python programs on your own computer?
+Need to install Python Extension and interpreter
+
+"""
+#Part 2: Coding Exercise - Your First Python Program
+print("Welcome to Python Learning!")
+print("hello world")
+print("Welcome to python programming")
+#Part 3: Research Exercise - Interactive Greeting Script
+name=input("What is your name? ")
+print("Hello, " + name + " Glad to have you learning Python.")
